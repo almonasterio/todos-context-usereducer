c7904d10df520772461f850b062130d4832ca51f
almonasterio <a.monasterio.a@gmail.com>
2020-05-05 17:44:07 -0500

    refactored with DispapatchContext

--- src/contexts/todos.context.js
+++ src/contexts/todos.context.js
@@ -5,13 +5,18 @@ import useTodoState from "../hooks/useTodoState"
 const defaultTodos=[{id:1,task: "Mow the lawn",completed:false},{id:2, task: "Buy Soap",completed:false}]
 
 export const TodosContext =createContext();
+export const DispatchContext=createContext();
+
 
 export function TodosProvider(props){
    // const {todos,addTodo,removeTodo,toogleTodo,editTodos}=useTodoState(defaultTodos);
     const [todos,dispatch] = useReducer(todoReducer,defaultTodos);
     return (
-        <TodosContext.Provider value={{todos, dispatch}}>
+        <TodosContext.Provider value={todos}>
+        <DispatchContext.Provider value={dispatch}>
             {props.children}
+
+        </DispatchContext.Provider>
         </TodosContext.Provider>
     )
 
